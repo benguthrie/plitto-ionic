66fff18b68a32b3f7787e91c92bc0b2df4a11380
Diego Netto <diegormnetto@gmail.com>
2014-10-20 13:24:05 -0500

    Implement list creation and deletion

--- client/app/scripts/controllers.js
+++ client/app/scripts/controllers.js
@@ -29,15 +29,73 @@ angular.module('Plitto.controllers', [])
 .controller('ProfileCtrl', function($scope) {
 })
 
-.controller('ListsCtrl', function($scope) {
-  $scope.lists = [
-    { title: 'Programming', id: 1 },
-    { title: 'Wakeboarding', id: 2 },
-    { title: 'Startups', id: 3 },
-    { title: 'Chasing $$ signs', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Watching the Cowboys lose', id: 6 }
-  ];
+.controller('ListsCtrl', function($scope, $ionicModal, $ionicActionSheet, LoopBackAuth, User) {
+  // Initialize variablse
+  $scope.newList = {};
+  $scope.modal = null;
+  $ionicModal.fromTemplateUrl('templates/modals/add-list.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  User.lists({ id: LoopBackAuth.currentUserId },
+    function(lists) {
+      $scope.lists = lists;
+    }, function(err) {
+      console.log('fail', err);
+    });
+
+  // Launch add-list modal
+  $scope.addListModal = function () {
+    $scope.modal.show();
+  };
+
+  // Close add-list modal
+  $scope.closeModal = function () {
+    $scope.modal.hide();
+  };
+
+  // Create a new list
+  $scope.createList = function () {
+    User.lists.create({ id: LoopBackAuth.currentUserId },
+      $scope.newList,
+      function(list) {
+      $scope.lists.push(list);
+      $scope.newList = {};
+      $scope.closeModal();
+    }, function (err) {
+      console.log('fail', err);
+    });
+  };
+
+  // Delete a list
+  $scope.deleteList = function (list) {
+    $ionicActionSheet.show({
+      destructiveText: 'Delete',
+      titleText: 'Are you sure you want to delete this list?',
+      cancelText: 'Cancel',
+      cancel: function () {
+        console.log('Cancelled');
+      },
+      destructiveButtonClicked: function () {
+        User.lists.destroyById({
+          id: list.userId,
+          fk: list.id
+        }, function(deletedList) {
+          _.remove($scope.lists, list);
+        }, function(err) {
+          console.log('fail', err);
+        });
+        return true;
+      }
+    });
+  };
 })
 
 .controller('ListCtrl', function($scope, $stateParams) {
