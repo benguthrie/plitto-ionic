34c979f5c9b705ba2dfe9de81d06bb37b91c551f
Ben Guthrie <guthrie.ben@gmail.com>
2015-01-16 14:10:38 -0600

    Updated two batch files for android building.

--- client/buildAndroid.bat
+++ client/buildAndroid.bat
@@ -1,4 +1,11 @@
 cd C:\xampp\htdocs\plitto-ionic\client
+pause;
 grunt init
+pause;
+grunt clean
+grunt wiredep
+grunt init
+grunt compress
+cordova build android --release
 cordova run android
 pause;
---
+++ client/updatePath.bat
@@ -0,0 +1,10 @@
+
+set PATH=%PATH%;C:\apache-ant-1.9.4; C:\apache-ant-1.9.4\bin;
+ 
+ 
+set PATH=%PATH%;%ANT_HOME%\bin;
+
+
+set ANDROID_HOME=C:\AndroidSDK;
+
+set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;
